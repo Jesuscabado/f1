9c8ea814d820f704c1ee5b01169b4170abc74921
Jon <jonzapatab@gmail.comgit config --global user.name JonZapataB>
2023-03-08 17:23:19 +0100

    cambios html y css nuevo

--- index.js
+++ index.js
@@ -20,18 +20,20 @@ function filtrarF1(){
     });  
 }
 
-/* function filterList() {
-    var input, filter, ul, li, txtValue;
-    input = document.getElementById("buscadorF1");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("ul");
-    li = ul.getElementsByTagName("li");
-    for (var i = 0; i < li.length; i++) {
-      txtValue = li[i].textContent || li[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+let menuVer = false;
+//funcion que oculta o muestra el menu
+function mostrarOocultarmenu(){
+    if(menuVer){
+        document.getElementById("barrainicio").classList = "";
+        menuVer = false;
+    }else{
+        document.getElementById("barrainicio").classList = "titulos";
+        menuVer = true;
     }
-  } */
+}
+//ocultar menu una vez selecionas una opcion
+function seleccionar(){
+    document.getElementById("barrainicio").classList = "";
+    menuVer = false;
+
+}
