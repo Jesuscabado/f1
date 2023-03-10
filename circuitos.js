1a00da258dccd6d5f60d3ba5099985b49ce4dbb9
Jesuscabado <jesuscabado@hotmail.com>
2023-03-10 11:36:09 +0100

    añadido circuitos.css

--- circuitos.js
+++ circuitos.js
@@ -4,35 +4,53 @@ fetch('http://ergast.com/api/f1/2023/circuits.json')
     const circuits = data.MRData.CircuitTable.Circuits;
     circuits.forEach(circuit=> {
       console.log(circuit);
-      crearCircuitos( circuit.circuitId, circuit.circuitName, circuit.url, circuit.Location.locality, circuit.Location.country);
+      crearCircuito( circuit.circuitId, circuit.circuitName, circuit.url, circuit.Location.locality, circuit.Location.country);
     });
   })
   .catch(error => {
     console.log(error);
   });
 
-  function crearCircuitos(id, nombre, enlace, ubicacion, pais){
+  function crearCircuito(id, nombre, enlace, ubicacion, pais){
     let ul = document.createElement("ul");
-    let li = document.createElement("li");
-    let li1 = document.createElement("li");
-    let li2 = document.createElement("li");
-    let li3 = document.createElement("li");
+     ul.setAttribute("id", "circuitos"); 
+    
+    let circuito = document.createElement("li");
+    let wikipedia = document.createElement("li");
+    let localizacion = document.createElement("li");
+    let nacion = document.createElement("li");
     let li4 = document.createElement("li");
     let img = document.createElement("img");
     let link = document.createElement("a");
     link.href = enlace;
     link.setAttribute("target", "_blank");
-    link.innerText = "wikipedia";
+    link.innerText = "INFO";
     img.setAttribute("src", `circuitos/${id}.png`);
-    li.innerText = nombre;
-    li1.appendChild(link);
-    li2.innerText = ubicacion;
-    li3.innerText = pais;
+    circuito.innerText = nombre;
+    wikipedia.appendChild(link);
+    localizacion.innerText = ubicacion;
+    nacion.innerText = pais;
     li4.appendChild(img);
     ul.appendChild(img);
-    ul.appendChild(li);
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    ul.appendChild(li3);
+    ul.appendChild(circuito);
+    ul.appendChild(wikipedia);
+    ul.appendChild(localizacion);
+    ul.appendChild(nacion);
     document.getElementById("circuits").appendChild(ul);
 }
+
+/* intento carrusel 
+const carousel = document.querySelector("circuits");
+const images = document.querySelectorAll("circuitos");
+
+let index = 0;
+
+function nextImage() {
+  index++;
+  if (index > circuitos.length - 1) {
+    index = 0;
+  }
+  carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(nextImage, 3000); */
