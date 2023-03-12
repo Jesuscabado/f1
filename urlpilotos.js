9dd9e2cdd7c7744da1b0ee33bb5bf22d237cd240
Jon <jonzapatab@gmail.comgit config --global user.name JonZapataB>
2023-03-13 00:18:51 +0100

    html cambiado

--- urlpilotos.js
+++
@@ -1,6 +0,0 @@
-encodeURI("https://www.formula1.com/en/drivers.html");
-let driverName = result.Driver.givenName;
-let driverFamilyName = result.Driver.familyName;
-let param = encodeURIComponent(givenName & familyName);
-let url = "https://www.formula1.com/en/drivers/" + param + ".html";
-console.log(url);
